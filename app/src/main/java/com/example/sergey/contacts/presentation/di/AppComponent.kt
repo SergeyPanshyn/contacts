12c9cb2dbcac126e27892caadaafc1c8cf125cf5
panshyn.s@gmail.com <panshyn.s@gmail.com>
2018-08-08 09:59:18 +0300

    Implemented login via Google.

--- app/src/main/java/com/example/sergey/contacts/presentation/di/AppComponent.kt
+++ app/src/main/java/com/example/sergey/contacts/presentation/di/AppComponent.kt
@@ -3,8 +3,6 @@ package com.example.sergey.contacts.presentation.di
 import com.example.sergey.contacts.data.di.DataModule
 import com.example.sergey.contacts.data.di.RepositoriesModule
 import com.example.sergey.contacts.presentation.ContactsApp
-import com.example.sergey.contacts.presentation.login.di.LoginComponent
-import com.example.sergey.contacts.presentation.login.di.LoginModule
 import com.example.sergey.contacts.presentation.main.add.di.AddContactComponent
 import com.example.sergey.contacts.presentation.main.add.di.AddContactModule
 import com.example.sergey.contacts.presentation.main.list.di.ListComponent
@@ -15,8 +13,6 @@ import javax.inject.Singleton
 @Component(modules = arrayOf(AppModule::class, DataModule::class, RepositoriesModule::class))
 @Singleton
 interface AppComponent {
-
-    fun provideLoginComponent(loginModule: LoginModule): LoginComponent
 
     fun provideListComponent(listModule: ListModule): ListComponent
 
--- app/src/main/java/com/example/sergey/contacts/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/sergey/contacts/presentation/login/LoginActivity.kt
@@ -2,28 +2,75 @@ package com.example.sergey.contacts.presentation.login
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import butterknife.BindView
 import com.example.sergey.contacts.R
-import com.example.sergey.contacts.presentation.ContactsApp
-import com.example.sergey.contacts.presentation.login.di.LoginModule
-import javax.inject.Inject
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import android.content.Intent
+import android.widget.Toast
+import butterknife.ButterKnife
+import com.example.sergey.contacts.extension.setLoggedIn
+import com.example.sergey.contacts.presentation.main.MainActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.common.api.ApiException
 
-class LoginActivity : AppCompatActivity(), LoginPresenter.LoginView {
+class LoginActivity : AppCompatActivity() {
 
-    val loginComponent by lazy { ContactsApp.appComponent?.provideLoginComponent(LoginModule()) }
+    @BindView(R.id.sign_in_button)
+    lateinit var signInButton: SignInButton
 
-    @Inject
-    lateinit var loginPresenter: LoginPresenter<LoginPresenter.LoginView>
+    private val SIGN_IN_REQUEST_CODE: Int = 123
+
+    private var googleSignInClient: GoogleSignInClient? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
 
-        daggerInit()
+        ButterKnife.bind(this)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        signInButton.setSize(SignInButton.SIZE_STANDARD)
+        signInButton.setOnClickListener { signIn() }
 
     }
 
-    private fun daggerInit() {
-        loginComponent?.inject(this)
-        loginPresenter.setView(this)
+    private fun signIn() {
+        val signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, SIGN_IN_REQUEST_CODE)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_REQUEST_CODE) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            Toast.makeText(this, "Successfully signed in with account ${account.email} ", Toast.LENGTH_SHORT).show()
+
+            handleSuccessfulSignIn()
+        } catch (e: ApiException) {
+            Toast.makeText(this, "Error sign in", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun handleSuccessfulSignIn() {
+        this.setLoggedIn()
+
+        startActivity(Intent(this, MainActivity::class.java))
     }
 }
